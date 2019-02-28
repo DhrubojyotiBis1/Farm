7adf488c1dc69e8b5f3ac151bc611b4a9fa61d1f
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2019-02-28 21:13:16 +0530

    farmActivity tableView dynamic cell hight feature complete

--- Farm/Controller/adminClasses/adminFarmActivity.swift
+++ Farm/Controller/adminClasses/adminFarmActivity.swift
@@ -8,17 +8,33 @@
 
 import UIKit
 
-class adminFarmActivity: UIViewController {
+class adminFarmActivity: UIViewController ,UITableViewDelegate,UITableViewDataSource{
 
+    let content = ["hdsgjhagdhagdhgajhgshgdhajsgdajhgdhagdhasghgdjhagsdgjhgajsdhsgdhjsgfhfghsdfghjsdgfhjgjhsdgfhgsdhgsjhgfhsjgfhjfgsjhghfgsjhghjsgdjhfghjdsgfhjgfshjdghjfghjdgsjhfgshdgjhdfgshjgfhjsgfjhgfhjsdghjdgshjgfhjdghjfgsj","jadhjkdhjashdhjsaghdjgajhdgahsjdgahjdghajghdgahjdgahgdajhgsdja","jdashdhajsdhjjahajdhjsdghjagshdgsfdhgafgsfdghsfdhgafsghfaghsfdghasfaghdfhasfhgdfsaghfdhgdfaghsfdhgfdghfasgfahsgfahgdfghdfshgdfahgsdfghafshgdfsahgdfagda","sdhjahjdhajsgdahjgdjhagsjh"]
+    let name = ["sdfagsdfh","dsaghgsadgdfhsdjfgjhsdgjhjfhjshghgfhjsgfhsghdgjfgsjhdgfhjgsjfhgfsjhgfjhsgfshjfghfgshjfgshjghjfgj","dasjahjsgdahddasd","asdhshjdgadhjg"]
+    
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         configureTableView()
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "adminFarmActivityCell", for: indexPath) as! adminFarmActivityCell
+        cell.farmDescription.text = content[indexPath.row]
+        cell.name.text = name[indexPath.row]
+        
+        return cell
+    }
+    
     func configureTableView(){
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 120.0
