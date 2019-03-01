bdc773e244270e4d5e4cfec8cedd92a1ae97a68f
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2019-03-01 23:20:28 +0530

    auditorFarm data display complete

--- Farm/Controller/Custom Cells/adminFarmCell.swift
+++ Farm/Controller/Custom Cells/adminFarmCell.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class adminFarmCell: UITableViewCell {
 
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var farmSize: UILabel!
+    @IBOutlet weak var descriptions: UILabel!
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var name: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -19,6 +24,14 @@ class adminFarmCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    override func layoutSubviews() {
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = false
+        view.layer.shadowColor = UIColor.gray.cgColor
+        view.layer.shadowOffset = CGSize(width: 0, height: 3)
+        view.layer.shadowOpacity = 0.8
     }
 
 }
--- Farm/Controller/adminClasses/adminFarm.swift
+++ Farm/Controller/adminClasses/adminFarm.swift
@@ -7,20 +7,163 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SVProgressHUD
+import CoreLocation
 
-class adminFarm: UIViewController {
-
+class adminFarm: UIViewController,UITableViewDelegate,UITableViewDataSource{
+    
+    var farmName = [String]()
+    var farmSize = [String]()
+    var descriptions = [String]()
+    var location = [String]()
+    //TODO: DispatchGroup is used to wait untill the data is fetch back
+    let dispatchGroup = DispatchGroup()
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         configureTableView()
+        tableView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        tableView.dataSource = self
+        tableView.delegate = self
+        networking()
     }
     
+    private func networking(){
+        //TODO: Networking is done here :
+        SVProgressHUD.show()
+        let url = URL()
+        Alamofire.request(url.dataUrl).responseJSON { (response) in
+            if response.result.isSuccess{
+                let userjson:JSON = JSON(response.result.value!)
+                self.dataParsing(json: userjson)
+            }else{
+                print("Error")
+                self.showAlertForError(withMessage: "Check your internet connection")
+            }
+        }
+    }
+    
+    private func dataParsing(json : JSON){
+        for  i in 0...(json["add_f"].count - 1){
+            farmName.append(json["add_f"][i]["farm_n"].string!)
+            farmSize.append(json["add_f"][i]["farm_s"].string!)
+            descriptions.append(json["add_f"][i]["farm_d"].string!)
+            let latitude = json["add_f"][i]["farmla"].string!
+            let longitude = json["add_f"][i]["farmlo"].string!
+            self.getAddressFromLatLon(pdblLatitude:latitude , withLongitude: longitude )
+        }
+        dispatchGroup.notify(queue: .main) {
+            //TODO: run only when the dispatch group does not have anything
+            self.tableView.reloadData()
+            SVProgressHUD.dismiss()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return farmName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "auditorFarmCell", for: indexPath) as! adminFarmCell
+        
+        cell.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        cell.name.text = "Name: "+farmName[indexPath.row]
+        cell.farmSize.text = "Farmsize: "+farmSize[indexPath.row]
+        cell.descriptions.text = "Description: " + descriptions[indexPath.row]
+        if location.isEmpty == false {
+            cell.location.text = "Location: " + location[indexPath.row]
+        }
+        return cell
+        
+    }
+    
+    private func getAddressFromLatLon(pdblLatitude: String, withLongitude pdblLongitude: String) {
+        if ((Double("\(pdblLatitude)") != nil) && (Double("\(pdblLongitude)") != nil)){
+            
+            var center : CLLocationCoordinate2D = CLLocationCoordinate2D()
+            let lat: Double = Double("\(pdblLatitude)")!
+            //21.228124
+            let lon: Double = Double("\(pdblLongitude)")!
+            //72.833770
+            let ceo: CLGeocoder = CLGeocoder()
+            center.latitude = lat
+            center.longitude = lon
+            
+            let loc: CLLocation = CLLocation(latitude:center.latitude, longitude: center.longitude)
+            var addressString : String = ""
+            dispatchGroup.enter()//TODO: start's waiting for data here:
+            ceo.reverseGeocodeLocation(loc, completionHandler:
+                {(placemarks, error) in
+                    if (error != nil)
+                    {
+                        print("reverse geodcode fail: \(error!.localizedDescription)")
+                    }
+                    let pm = placemarks! as [CLPlacemark]
+                    
+                    if pm.count > 0 {
+                        let pm = placemarks![0]
+//                        print(pm.country)
+//                        print(pm.locality)
+//                        print(pm.subLocality)
+//                        print(pm.thoroughfare)
+//                        print(pm.postalCode)
+//                        print(pm.subThoroughfare)
+                        if pm.subLocality != nil {
+                            addressString = addressString + pm.subLocality! + ", "
+                        }
+                        if pm.thoroughfare != nil {
+                            addressString = addressString + pm.thoroughfare! + ", "
+                        }
+                        if pm.locality != nil {
+                            addressString = addressString + pm.locality! + ", "
+                        }
+                        if pm.country != nil {
+                            addressString = addressString + pm.country! + ", "
+                        }
+                        if pm.postalCode != nil {
+                            addressString = addressString + pm.postalCode! + " "
+                        }
+                        
+                        print(addressString)
+                    }
+                    self.dispatchGroup.leave()//TODO: stop's waiting
 
-    func configureTableView(){
-//        tableView.rowHeight = UITableView.automaticDimension
-//        tableView.estimatedRowHeight = 120.0
+                    print("inside")
+            })
+            dispatchGroup.notify(queue: .main) {
+                //TODO: run only when the dispatch group does not have anything
+                print("outside")
+                if addressString == "" {
+                    self.location.append("Error!")
+                }else{
+                    self.location.append(addressString)
+                }
+            }
+        }else{
+            location.append("Error!")
+        }
+        
+    }
+
+    private func configureTableView(){
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 120.0
+    }
+    
+    private func showAlertForError(withMessage message : String){
+        //TODO: check wether username or password enntered is wrong:
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let reEnter = UIAlertAction(title: "Retry", style: .default) { (action) in
+            self.networking()
+        }
+        alert.addAction(reEnter)
+        present(alert, animated: true, completion: nil)
+        SVProgressHUD.dismiss()
     }
 
 }
